6ed0d93832976363d829272d5d86ac7301f33d8c
root <root@DESKTOP-99KOHJ0>
2024-08-14 20:36:38 +0300

    add lint

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from "readline-sync";
 
-export const userName = (user) => {
+export const userName = () => {
   const name = readlineSync.question("May I have your name?");
   return `Hello, ${name}!`;
 };
